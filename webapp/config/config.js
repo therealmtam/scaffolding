32eafe09459d2087d00f2b7f514a7e70e883113b
Max Tam <max.c.tam@gmail.com>
2018-01-30 10:25:31 -0800

    included config file scaffold & documentation

---
+++ webapp/config/config.js
@@ -0,0 +1,24 @@
+// CONFIG:
+/*
+  This file contains private configurations that are not to be
+  publicly published such as API keys.
+
+  Example:
+  db: {
+    host: 'localhost',
+    username: 'Joe',
+    password: '1234',
+    database: 'postgres_db_1', // this is the database name
+    dialect: 'postgres',  //keep this as postgres
+  },
+*/
+
+module.exports = {
+  db: {
+    host: '',
+    username: '',
+    password: '',
+    database: '',
+    dialect: '',
+  },
+};
